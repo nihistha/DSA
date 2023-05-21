d68262d00bda6aca7e82b58f72c6262b3328de9f
nihistha <nihirastha20@gmail.com>
2023-05-21 07:55:33 +0545

    changes

--- MergeSort.java
+++ MergeSort.java
@@ -1,34 +1,64 @@
 import javax.xml.stream.events.StartElement;
+//O(n log n)
 
 public class MergeSort {
     public void merge(int arr[],int start,int mid,int end){
-        int len = arr.length;
-        int n1 = mid -start +1;
-        int n2 = end - mid;
-        int l[] = new int[len];
-        int r[] = new int[len];
+        int len = arr.length; 
+        int n1 = mid -start +1; // the length will be 3
+        int n2 = end - mid; //the length will be four
+        int l[] = new int[n1]; 
+        int r[] = new int[n2];
 
         for(int i = 0;i<n1;i++){
-            l[i]=arr[i];
+            l[i]=arr[start +i]; // puts the value of start + i  in l[i]
         }
         for(int j = 0;j<n2;j++){
-            r[j]=arr[mid+1+j];
+            r[j]=arr[mid+1+j]; // puts the values after mid in r[j]
         }
 
         int i = 0;
         int j = 0;
-        int k = start;
+        int k = start; //start = 0
         while(i<n1 && j<n2){
             if (l[i]<=r[j]){
-                arr[k] =l[i];
+                arr[k] =l[i]; //
+                i++;
+            }else{
+                arr[k] = r[j];
+                j++;
             }
+            k++;
+        }
+        while (i<n1){
+            arr[k] = l[i];
+            i++;
+            k++;
+        }
+        while(j<n2){
+            arr[k] =r[j];
+            j++;
+            k++;
         }
         
     }
     public void mergeSort(int a[],int start,int end){
+        if(start<end){
         int mid = (start + end )/2;
         mergeSort(a,start,mid);
         mergeSort(a, mid+1, end);
+        merge(a,start,mid,end);
+        }
         
     } 
+    public void print(int arr[]){
+        for (int i = 0; i<arr.length-1;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        MergeSort m = new MergeSort();
+        int arr[] = {23,3,55,2,1,0};
+        m.mergeSort(arr,0,arr.length-1);
+        m.print(arr);
+    }
 }
--- Target.java
+++ Target.java
@@ -11,6 +11,32 @@ public class Target {
         }
         return coin(n-1) +coin (n-2) + coin(n-3);
     }
+
+    public int combination(int arr[],int n){
+        int dp[][] =new int [arr.length+1][n+1];
+        for(int i =0;i<arr.length+1;i++){
+            dp[i][0] = 1;
+        }
+        for(int i= 1;i<arr.length+1;i++){
+            for(int j = 1;j<n+1;j++){
+                if(arr[i-1]>j){
+                    dp[i][j] = dp[i-1][j];
+                }else if(arr[i-1]<=j){
+                    int diff = j-arr[i-1];
+                    dp[i][j] = dp[i-1][j] + dp[i][diff];
+                }
+            }
+        }
+        return dp[dp.length-1][dp[0].length-1];
+    }
+
+    //find minimum no of coins required to make a targeted value
+    //grid traveller problem
+    
+    //target = 6
+    //minimum = 3+3
+    
+
     public static void main(String[] args) {
         System.out.println(coin(6)); 
     }
